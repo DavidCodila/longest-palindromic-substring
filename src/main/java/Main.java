e2ea82382072f04dfb731128e1a8f6ed9aef517a
David Codila <david.codila@auspost.com.au>
2025-02-18 18:10:44 +1100

    add passing code for isPalindrome

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,11 @@ public class Main {
     }
 
     public static boolean isPalindrome(String s) {
+        for(int i = 0; i <= s.length() / 2; i++) {
+           if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+               return false;
+           }
+        }
         return true;
     }
 }
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TestMain {
 
     @Test
-    public void testIsPalindrome() {
+    public void testIsPalindrome_aba() {
         assertTrue(Main.isPalindrome("aba"));
+    }
+
+    @Test
+    public void testIsPalindrome_abb() {
+        assertFalse(Main.isPalindrome("abb"));
     }
 }
